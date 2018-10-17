18bb9607f88de35fd10490bfacbb0d30bf16358e
Ricardo Trejos <cardotrejos@gmail.com>
2018-10-17 14:12:29 -0500

    Add changes to app
    Add lyrics creation and list , add thumbs up and finishing some details.

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -7,14 +7,16 @@ import query from '../queries/fetchSongs';
 class SongList extends Component {
 	onSongDelete(id){
 		this.props.mutate({ variables: { id } })
-
+			.then(() => this.props.data.refetch());
 	}
 
 	renderSongs() {
 		return this.props.data.songs.map(({ id, title }) => {
 			return (
 				<li key={id} className="collection-item">
+				<Link to={`/songs/${id}`}>
 					{title}
+				</Link>
 				<i
 				  className="material-icons"
 				  onClick={() => this.onSongDelete(id)}
